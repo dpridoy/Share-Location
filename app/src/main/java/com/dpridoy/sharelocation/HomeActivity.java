c4b97b407704e3c8e1dff4ad9aa92de3fd8e5274
Nasirul Haque Ridoy <dpridoy@gmail.com>
2019-11-24 19:08:39 +0600

    second commit creating loginView

---
+++ app/src/main/java/com/dpridoy/sharelocation/HomeActivity.java
@@ -0,0 +1,82 @@
+package com.dpridoy.sharelocation;
+
+import android.os.Bundle;
+
+import com.dpridoy.sharelocation.ui.gallery.GalleryFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new GalleryFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_find_people);
+        }
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id=menuItem.getItemId();
+        if (id==R.id.nav_find_people){
+
+        }else if (id==R.id.nav_add_people){
+
+        }else if(id==R.id.nav_sign_out){
+
+        }
+//        DrawerLayout drawer=(DrawerLayout)findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ app/src/main/java/com/dpridoy/sharelocation/Utils/Common.java
@@ -0,0 +1,12 @@
+package com.dpridoy.sharelocation.Utils;
+
+import com.dpridoy.sharelocation.Model.User;
+
+public class Common {
+    public static final String USER_INFORMATION="UserInformation";
+    public static final String USER_UID_SAVE_KEY = "SaveUid";
+    public static final String TOKKENS = "Tokens";
+
+    public static User loggedUser;
+
+}
